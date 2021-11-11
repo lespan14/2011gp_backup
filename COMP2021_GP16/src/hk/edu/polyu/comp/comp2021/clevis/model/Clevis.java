5f1b07eabfa1dbfec22c92e0536303cc28b52f72
gardenia06 <93461345+gardenia06@users.noreply.github.com>
2021-11-12 00:23:39 +0800

    Update Clevis.java

--- COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/model/Clevis.java
+++ COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/model/Clevis.java
@@ -1,10 +1,14 @@
 package hk.edu.polyu.comp.comp2021.clevis.model;
-import java.awt.BorderLayout;
+/*import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Canvas;
-import java.awt.Graphics;
+import java.awt.Graphics;*/
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -53,24 +57,40 @@ public class Clevis {
     	this.frame.pack();
     }
     
-
-    
+	
+	class NameShape {
+            private String name;
+            private Shape shape;
+            public NameShape (String name, Shape shape){
+            this.name=name;
+            this.shape=shape;
+        }
+        public String getname(){return name;}
+        public Shape getshape(){return shape;}
+        }
+  
 }
+	public List<NameShape> shapes= new ArrayList<>();
 
-/*
-public void rectangle (String n, int x, int y, int w, int h){
-        g.drawRect(x, y, w, h);
-    }
+	public void rectangle (String n, double x, double y, double w, double h){
+        	// g.drawRect(x, y, w, h);
+		shapes.add(new NameShape(n, new Rectangle2D.Double(x, y, w, h)));
+    	}
 
-public void circle (String n, int x, int y, int r){
-        g.drawOval(x, y, r, r);
-    }
+	public void circle (String n, double x, double y, double r){
+        	// g.drawOval(x, y, r, r);
+		shapes.add(new NameShape(n, new Ellipse2D.Double(x, y, r, r)));
+    	}
     
-public void square (String n, int x, int y, int l){
-        g.drawRect(x, y, l, l);
-    }
+	public void square (String n, double x, double y, double l){
+       	 	// g.drawRect(x, y, l, l);
+		shapes.add(new NameShape(n, new Rectangle2D.Double(x, y, l, l)));
+   	}
 
-public void line (String n, int x1, int y1, int x2, int y2){
-        g.drawLine(x1, y1, x2, y2);
-    }
-*/
+	public void line (String n, double x1, double y1, double x2, double y2){
+        	// g.drawLine(x1, y1, x2, y2);
+		shapes.add(new NameShape(n, new Line2D.Double(x1, y1, x2, y2)));
+    	}
+
+
+//ref: https://stackoverflow.com/questions/28839765/how-do-i-create-an-object-of-an-arraylist-shape-in-java
