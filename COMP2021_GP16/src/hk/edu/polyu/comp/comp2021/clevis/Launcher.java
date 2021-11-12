aa2386077e9348022a9517fcff5b4e35b8ff26d2
HolinLam <lholincc@gmail.com>
2021-11-12 16:34:34 +0800

    Logger can now create an empty log txt in the project folder on launch

--- COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/Launcher.java
+++ COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/Launcher.java
@@ -2,11 +2,16 @@ package hk.edu.polyu.comp.comp2021.clevis;
 
 import hk.edu.polyu.comp.comp2021.clevis.model.Clevis;
 import hk.edu.polyu.comp.comp2021.clevis.Application;
+import hk.edu.polyu.comp.comp2021.clevis.log.Log;
 
 public class Launcher {
     public static void main(String[] args){
+    	Log logger = new Log("", "LogTest.txt");
+    	//System.out.println("logger creation done");
+    	
     	Application game = new Application("Test", 500, 800);
     	game.start();
+    	
         // Initialize and utilize the system
     }
 }
--- COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/log/Log.java
+++ COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/log/Log.java
@@ -1,5 +1,34 @@
 package hk.edu.polyu.comp.comp2021.clevis.log;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Log {
-
+	private String filename;
+	private String htmlname;
+	
+	public Log(String htmlname, String filename) {
+		this.filename = filename;
+		this.htmlname = htmlname;
+		try {
+			String currentPath = new java.io.File(".").getCanonicalPath();
+			File logFile = new File(currentPath + "\\" + this.filename);
+			logFile.setWritable(true);
+			logFile.setReadable(true);
+			
+			//System.out.println("creating file as " + currentPath + "\\" + this.filename);
+			
+			FileWriter logger = new FileWriter(logFile);
+			logger.write("overwritten");
+			//System.out.println("written");
+			logger.close();
+		} catch (IOException e) {
+			System.out.println("error");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 }
+
