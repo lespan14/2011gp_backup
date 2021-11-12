9598d78304568a49cab2b715dfab31d0ad3f5912
gardenia06 <93461345+gardenia06@users.noreply.github.com>
2021-11-12 18:32:08 +0800

    Update Clevis.java

--- COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/model/Clevis.java
+++ COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/model/Clevis.java
@@ -8,6 +8,7 @@ import java.awt.Graphics;*/
 import java.awt.*;
 import java.awt.geom.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JFrame;
