539db10da90877fae4e64bfd4030133f9c0e0757
HolinLam <74394753+HolinLam@users.noreply.github.com>
2021-11-16 00:19:36 +0800

    Updated input

--- COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/Application.java
+++ COMP2021_GP16/src/hk/edu/polyu/comp/comp2021/clevis/Application.java
@@ -5,25 +5,102 @@ import hk.edu.polyu.comp.comp2021.clevis.model.Clevis;
 
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.io.*;
+import java.io.Console;
 
-public class Application implements Runnable{
+public class Application{
 	
-	private Thread thread;
-	public int width, height;
-	String title;
-	private boolean running = false;
+	//private Thread thread;
+	//public int width, height;
+	//String title;
+	//private boolean running = false;
 	private Clevis clevis;
-	BufferStrategy bs;
-	public Graphics g;
+	//BufferStrategy bs;
+	//public Graphics g;
 	
-	public Application(String title, int width, int height) {
-		this.width = width;
-		this.height = height;
-		this.title = title;
-		clevis = new Clevis(title, width, height);
+	public Application() {
+		clevis = new Clevis();
+		Console console = System.console();
+		while (true) {
+			menu();
+			String input = "";
+			String command ="";
+			input = console.readLine();
+			String[] tokens = input.split("\\s+");
+			command = tokens[0];
+			
+			switch (command) {
+			case "rectangle" :
+				String recname = tokens[1];
+				double rex = Double.parseDouble(tokens[2]);
+				double rey = Double.parseDouble(tokens[3]);
+				double rewidth = Double.parseDouble(tokens[4]);
+				double reheight = Double.parseDouble(tokens[5]);
+				clevis.rectangle(recname, rex, rey, rewidth, reheight);
+				break;
+			case "line" :
+				String linename = tokens[1];
+				double lix1 = Double.parseDouble(tokens[2]);
+				double liy1 = Double.parseDouble(tokens[3]);
+				double lix2 = Double.parseDouble(tokens[4]);
+				double liy2 = Double.parseDouble(tokens[5]);
+				clevis.line(linename, lix1, liy1, lix2, liy2);
+				break;
+			case "circle" :
+				String cirname = tokens[1];
+				double cix = Double.parseDouble(tokens[2]);
+				double ciy = Double.parseDouble(tokens[3]);
+				double cirad = Double.parseDouble(tokens[4]);
+				clevis.circle(cirname, cix, ciy, cirad);
+				break;
+			case "square" :
+				break;
+			case "group" :
+				break;
+			case "ungroup":
+				break;
+			case "delete":
+				break;
+			case "boundingbox" :
+				break;
+			case "move" :
+				break;
+			case "intersect" :
+				break;
+			case "quit" :
+				break;
+			default :
+				System.out.println("Invalid input, please input again\n");
+				break;
+				
+			}
+			
+			
+		}
+		
 	} 
+	
+	
+	
+	
+	//print menu screen
+	private static void menu() {
+		System.out.println("Standing by for new command...");
+		System.out.print(">_");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
-    
+    /*
     private void inital() {
     	
     }
@@ -80,4 +157,5 @@ public class Application implements Runnable{
     		e.printStackTrace();
     	}
     }
+    */
 }
